40e8976b6a182427f1671f21420dbc888bf8d125
Adam Bíl <adam.bil@hotmail.cz>
2019-07-01 00:17:15 +0200

    bs s importem globalu TODO

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
     pluginOptions: {
         "style-resources-loader": {
             preProcessor: "sass",
-            patterns: [path.resolve(__dirname, "./src/styles/global.sass")]
+            patterns: [path.resolve(__dirname, "src/styles/global.sass")],
+            data: `@import "src/styles/global.sass"`
         }
     }
 };
