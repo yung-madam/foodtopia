6ce4fab6abab7b467393a34153c9784bd9dd23a0
Adam Bíl <adam.bil@hotmail.cz>
2019-06-30 18:39:33 +0200

    basic server stuff

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
