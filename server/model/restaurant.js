d5f10b9477fa0a7d435725215852efdc1d9e85ad
Vojtěch Kraus <krausvo1@fit.cvut.cz>
2019-08-04 23:15:40 +0200

    Zakladni endpointy pro restaurace.

--- server/model/restaurant.js
+++ server/model/restaurant.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  stars: Number,
-  comment: String,
-  address: String,
-  dishes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Dish'}]
+  name: { type: String, required: true, maxlength: 150 },
+  address: { type: String, maxlength: 100 },
+  stars: { type: Number, min: 0, max: 5 },
+  comment: { type: String, maxlength: 250 },
+  dishes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dish' }]
 });
+
+restaurantSchema.methods.addDish = (dish) => {
+    this.dishes.push(dish);
+    this.save();
+    return this;
+};
 
 module.exports = mongoose.model('Restaurants', restaurantSchema);
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -4,20 +4,71 @@ const router = express.Router();
 
 // Internal dependencies
 const Restaurant = require('../model/restaurant');
+const Dish = require('../model/dish');
 
 /**
  * GET /api/restaurants
  */
 router.get('/restaurants', (req, res, next) => {
-    const restaurants = Restaurant.find();
-    res.send(restaurants);
+    Restaurant.find((err, restaurants) => {
+        res.status(200).json(restaurants);
+    });
+});
+
+/**
+ * GET /api/restaurants/:id
+ */
+router.get('/restaurants/:id', (req, res, next) => {
+    Restaurant.findOne({ _id: req.params.id }, (err, restaurant) => {
+        if(!restaurant) {
+            res.status(404).json('Tenhle pajzl nemáme!'); // 404?
+        }
+        restaurant.populate('dishes')
+            .then(result => res.status(200).json(result))
+            .catch(err => res.status(500).json(err));
+    });
 });
 
 /**
  * POST /api/restaurants
  */
 router.post('/restaurants', (req, res) => {
-    res.send('api vec');
+    const restaurant = new Restaurant({
+        name: req.name,
+        address: req.address,
+        stars: req.stars,
+        comment: req.comment,
+    });
+    restaurant.save()
+        .then(result => res.status(201).json(result))
+        .catch(err => res.status(400).json(err));
+});
+
+/**
+ * POST /api/restaurants/:id/dishes
+ * Create new Dish for given Restaurant.
+ */
+router.post('/restaurants/:id/dishes', (req, res) => {
+    if(!req.params.id){
+        res.json('dej id pls');
+    }
+    Restaurant.findOne({ _id: req.params.id }, (err, restaurant) => {
+        if(!restaurant) {
+            res.status(404).json('Tenhle pajzl nemáme!');
+        }
+        const dish = new Dish({
+            name: req.name,
+            stars: req.stars,
+            comment: req.comment,
+            price: req.price
+        });
+        dish.save()
+            .then(result => {
+                restaurant.addDish(result);
+                res.status(201).json(result);
+            })
+            .catch(err => res.status(400).json(err));
+    });
 });
 
 module.exports = router;
